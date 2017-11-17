b54e4af6a05f853138bb9eb544ab85a8bfd41158
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-17 16:19:06 +1300

    Using the GraphiQl tool

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,41 @@
 const graphql = require('graphql')
+const _ = require('lodash')
 
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql
 
-const UserType = new GraphQLObjectType ({
+const users = [
+  {id: '23', firstName: 'Mark', age: 29},
+  {id: '47', firstName: 'Bill', age: 36}
+
+]
+
+const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: {type: GraphQLString},
     firstName: {type: GraphQLString},
     age: {type: GraphQLInt}
   }
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}},
+      resolve (parentValue, args) {
+        return _.find(users, {id: args.id})
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
